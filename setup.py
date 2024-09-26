beee452111d45e853c913e2b7284892136d44f90
Petersonsantos19 <sdpeterson19@gmail.com>
2024-09-26 16:47:36 -0300

    Finalizando Setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="image processing Package using Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Petersonsantos19/image-processing-package"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
